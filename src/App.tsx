6c7917175f1ecbc43567c2a64a0f36861c4190d7
anna-7nova <anna.7nova@yandex.by>
2024-12-10 13:13:29 +0300

    my version to find a solution (not correct, but I tried)

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,67 @@
 import React, { useState } from 'react';
 import './App.css';
+import { UniversalButton } from './microtasks/UniversalButton';
+
+type MoneyType = {
+  banknotes: 'Dollars' | 'RUBLS' 
+  value: number
+  number: string 
+}
+
+//data
+const allList: Array<MoneyType> = [
+  { banknotes: 'Dollars', value: 100, number: ' a1234567890' },
+  { banknotes: 'Dollars', value: 50, number: ' z1234567890' },
+  { banknotes: 'RUBLS', value: 100, number: ' w1234567890' },
+  { banknotes: 'Dollars', value: 100, number: ' e1234567890' },
+  { banknotes: 'Dollars', value: 50, number: ' c1234567890' },
+  { banknotes: 'RUBLS', value: 100, number: ' r1234567890' },
+  { banknotes: 'Dollars', value: 50, number: ' x1234567890' },
+  { banknotes: 'RUBLS', value: 50, number: ' v1234567890' },
+]
 
 function App() {
-  // let initialCount = 0;  
-  const [count, setCount] = useState(0)
-  const onClickHandler1 = () => {
-    setCount(count+1); 
-  }
-  const onClickHandler2 = () => {
-    setCount(count-1)
-  }
+  const [money, setMoney] = useState(allList)
 
+  // const [nameOfButton, setNameOfButton] = useState("all")
+  
 
+  function filteredButton (title: string) {
+    if (title === 'All') {
+      return (
+        setMoney(allList)
+      )
+    }
+    if (title === 'Dollars') {
+      return (
+        setMoney(allList.filter(filteredMoney => filteredMoney.banknotes === 'Dollars' ))
+      )
+    }
+    if (title === 'RUBLS') {
+      return(
+        setMoney(allList.filter(filteredMoney => filteredMoney.banknotes === 'RUBLS' ))
+      )
+    }
+  }
 
   return (
     <div className="App">
-     <p>{count}</p>
-     <button onClick={onClickHandler1}>+</button>
-     <button onClick={onClickHandler2}>-</button>
+      <ul>
+        {money.map((objectFromArray) => {
+          return (
+            <div>
+              <li key={objectFromArray.number}>
+                <span>{objectFromArray.banknotes}</span>
+                <span>{objectFromArray.value}</span>
+                <span>{objectFromArray.number}</span>
+              </li>
+            </div>
+          )
+        })}
+      </ul>
+      <UniversalButton callBack={() => filteredButton('All')} title='All' />
+      <UniversalButton callBack={() => filteredButton('Dollars')} title='Dollars' />
+      <UniversalButton callBack={() => filteredButton('RUBLS')} title='Rubles' />
     </div>
   );
 }
@@ -25,15 +69,3 @@ function App() {
 export default App;
 
 
-// function App() {
-//   // const subscriber = (name: string) => console.log(name)
-
-//   return (
-//     <div className="App">
-//       {/* Hello, samurai! Let's go! */}
-//       {/* <UniversalButton title={"Youtube-chanel-1"} callBack={() => {subscriber("Anna")}}/>
-//       <UniversalButton title={"Youtube-chanel-2"} callBack={() => {subscriber("Viktor")}}/>
-//       <UniversalButton title={"Youtube-chanel-3"} callBack={() => {subscriber(" ")}}/> */}
-//     </div>
-//   );
-// }
